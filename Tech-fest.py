224593d13d0d74276c6d2e6d20edfd182fa827b4
Samuray360 <151565282+Samuray360@users.noreply.github.com>
2025-03-10 10:54:50 -0400

    Update Tech-fest.py

--- Tech-fest.py
+++ Tech-fest.py
@@ -1,2 +1,12 @@
 import flet as ft
 
+
+def main(page: ft.Page):
+    page.title = "Fisic calculator"
+    page.bgcolor = "white"
+    page.window.height = 1200
+    page.window.width = 1200
+    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
+    page.vertical_alignment = ft.MainAxisAlignment.START
+    page.add()
+    ft.app(target=main)
