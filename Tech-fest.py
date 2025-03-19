555e587996adbc89f753f5812799340400eb1e2f
Samuray360 <151565282+Samuray360@users.noreply.github.com>
2025-03-19 13:54:27 -0400

    gg

--- Tech-fest.py
+++ Tech-fest.py
@@ -9,6 +9,7 @@ def main(page: ft.Page):
     page.vertical_alignment = ft.MainAxisAlignment.START
 
     title=ft.Text("Physic's calculator",color="black",height=50) 
+    user_input=ft.TextField()
     
     def ohms_law(e):
         def calculate(e):
@@ -65,16 +66,56 @@ def main(page: ft.Page):
         page.dialog = dialog
         dialog.open = True
         page.update()
+    def equivalent_formula():
+        ft.Column([
+            ft.Container(
+            print(1),
+            ft.Divider(height=1,color="black"),
+            user_input
+            )
+        ])
+    ohms_law_page=ft.Container(
+       
+        width=page.width,
+        height=page.width,
         
-    def capacitance():#carlos , ethan
+    )
+    capacitance_page=ft.Container(   
+        equivalent_formula(),
+        width=page.width,
+        height=page.width,
+    )
+    resistance_page=ft.Container(   
+        equivalent_formula(),
+        width=page.width,
+        height=page.width,
+    )
+
+  
+
+    def ohms_law_view():#diego
+        ohms_law_page.visible=True
+        capacitance_page.visible=False
+        resistance_page.visible=False
+    def capacitance_view():#carlos , ethan
+        ohms_law_page.visible=False
+        capacitance_page.visible=True
+        resistance_page.visible=False
+        equivalent_formula
         return
-    def resistance():#carlos , ethan
+    def resistance_view():#carlos , ethan
+        ohms_law_page.visible=False
+        capacitance_page.visible=False
+        resistance_page.visible=True
+        equivalent_formula
         return
+    
+  
 
     
-    ohms_law_button=ft.ElevatedButton("Ohms law",on_click=ohms_law,width=200,height=50,style=ft.ButtonStyle(bgcolor="#849bff",color="white"),)
-    capacitance_button=ft.ElevatedButton("Capacitance",on_click=capacitance,width=200,height=50,style=ft.ButtonStyle(bgcolor="#849bff",color="white"))
-    resistance_button=ft.ElevatedButton("Resistance",on_click=resistance,width=200,height=50,style=ft.ButtonStyle(bgcolor="#849bff",color="white"))
+    ohms_law_button=ft.ElevatedButton("Ohms law",on_click=ohms_law_view,width=200,height=50,style=ft.ButtonStyle(bgcolor="#849bff",color="white"),)
+    capacitance_button=ft.ElevatedButton("Capacitance",on_click=capacitance_view,width=200,height=50,style=ft.ButtonStyle(bgcolor="#849bff",color="white"))
+    resistance_button=ft.ElevatedButton("Resistance",on_click=resistance_view,width=200,height=50,style=ft.ButtonStyle(bgcolor="#849bff",color="white"))
 
     button_column=ft.Column([ohms_law_button,capacitance_button,resistance_button],spacing=40)
 
